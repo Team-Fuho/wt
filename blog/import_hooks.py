b1db68b38b5739b31e06508a6814974a6037ef63
stdpi <stdpi@proton.me>
2024-06-06 07:13:20 +0000

    feat: import system
    wagtail_wordpress_import required dooring migration

---
+++ blog/import_hooks.py
@@ -0,0 +1,37 @@
+# get_or_save_image is a convenience function provided in the package
+from wagtail_wordpress_import.block_builder_defaults import get_or_save_image
+from .models import BlogPage
+
+def header_image_processor(imported_pages: BlogPage, data_tag, items_cache):
+    """
+    imported_pages:
+        Is a specific() page model queryset of all imported pages.
+    data_tag:
+        Is the value of the `DATA_TAG` key from the configuration above.
+    items_cache:
+        Is a list of dictionaries, one for each item in the XML file.
+    """
+
+    # See note above about leading _ and : characters in the XML value
+    lookup = f'wp_post_meta__{data_tag}'
+
+    for attachment in items_cache:
+        # The id of the cached item used in the filter
+        thumbnail_id = attachment.get('wp:post_id')
+
+        # Filter the imported_pages for only pages that include the
+        # matching thumbnail_id in the wp_post_meta field
+        pages = imported_pages.filter(**{lookup: thumbnail_id})
+
+        if pages.exists():
+            # guid is the url of the image to fetch, the get_or_save_image()
+            # function will fetch the image if it doesn't exist
+            image = get_or_save_image(attachment.get('guid'))
+
+            # update header_image field in all of the pages in
+            # the queryset with the image object
+            pages.update(thumb=image)
+
+            # the print statement below is optional
+            # and will show the progress in the console
+            print('Attaching header images to pages:', pages)
--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-from wagtail.admin.panels import FieldPanel
+from wagtail.fields import StreamField
+from wagtail.admin.panels import (
+    FieldPanel,
+    ObjectList,
+    TabbedInterface,
+)
 from wagtail.api import APIField
 from wagtail.search import index
 
 from base.models import TFImage, TFRenditionGroup
 
-# keep the definition of BlogIndexPage model, and add the BlogPage model:
+from wagtail_wordpress_import.blocks import WPImportStreamBlocks
+from wagtail_wordpress_import.models import WPImportedPageMixin
 
 
-class BlogPage(Page, TFRenditionGroup):
+class BlogPage(WPImportedPageMixin, Page, TFRenditionGroup):
     thumb = models.ForeignKey(
         TFImage,
         on_delete=models.SET_NULL,
@@ -20,7 +26,8 @@ class BlogPage(Page, TFRenditionGroup):
     )
 
     intro = models.CharField(max_length=250, blank=True)
-    body = RichTextField(blank=True)
+    # body = RichTextField(blank=True)
+    body = StreamField(WPImportStreamBlocks)
     localize_default_translation_mode = 'simple'
 
     search_fields = Page.search_fields + [
@@ -34,6 +41,15 @@ class BlogPage(Page, TFRenditionGroup):
         FieldPanel('intro'),
         FieldPanel('body'),
     ]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading='Content'),
+            ObjectList(Page.promote_panels, heading='Promote'),
+            ObjectList(Page.settings_panels, heading='Settings', classname='settings'),
+            ObjectList(WPImportedPageMixin.wordpress_panels, heading='Debug'),
+        ]
+    )
 
     @property
     def blog_date(self):
@@ -59,3 +75,25 @@ class BlogPage(Page, TFRenditionGroup):
         #     'authors'
         # ),  # This will nest the relevant BlogPageAuthor objects in the API response
     ]
+
+    def import_wordpress_data(self, data):
+        # Wagtail page model fields
+        self.title = data['title']
+        self.slug = data['slug']
+        self.first_published_at = data['first_published_at']
+        self.last_published_at = data['last_published_at']
+        self.latest_revision_created_at = data['latest_revision_created_at']
+        self.search_description = data['search_description']
+
+        # debug fields
+        self.wp_post_id = data['wp_post_id']
+        self.wp_post_type = data['wp_post_type']
+        self.wp_link = data['wp_link']
+        self.wp_raw_content = data['wp_raw_content']
+        self.wp_block_json = data['wp_block_json']
+        self.wp_processed_content = data['wp_processed_content']
+        self.wp_normalized_styles = data['wp_normalized_styles']
+        self.wp_post_meta = data['wp_post_meta']
+
+        # own model fields
+        self.body = data['body']
